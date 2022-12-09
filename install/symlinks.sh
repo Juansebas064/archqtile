2049722592ee935432bc860607da53f016990dce
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2022-12-09 15:01:25 -0500

    Delete .fonts from home directory

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -1,5 +1,6 @@
 cd ~
 rm ~/.zshrc ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 
+rm -r ~/.fonts
 ln -s ~/.config/.zshrc ~
 ln -s ~/.config/.fonts/ ~
 cp -r ~/.config/.oh-my-zsh ~
