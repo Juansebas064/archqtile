0188d11e1ec323d4305161d56b55add311def463
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-03-02 20:25:18 -0500

    Add xclipbard packages

--- install/packages.sh
+++ install/packages.sh
@@ -2,7 +2,7 @@ sudo pacman -S archlinux-keyring --noconfirm
 
 sudo pacman -Syyu --noconfirm
 
-sudo pacman -S xf86-input-libinput xorg-server xorg-server-common xorg-setxkbmap xorg-xev xorg-xrandr lightdm lightdm-webkit2-greeter ntfs-3g xdg-user-dirs networkmanager network-manager-applet pipewire-pulse playerctl brightnessctl blueman wget curl base-devel --noconfirm
+sudo pacman -S xf86-input-libinput xorg-server xorg-server-common xorg-setxkbmap xorg-xev xorg-xrandr lightdm lightdm-webkit2-greeter ntfs-3g xdg-user-dirs networkmanager network-manager-applet pipewire-pulse playerctl brightnessctl blueman wget curl base-devel xorg-xclipboard xclip --noconfirm
 
 sudo pacman -S qtile xterm zsh alacritty gparted firefox telegram-desktop gimp obs-studio thunar thunar-volman thunar-archive-plugin gvfs unzip unrar file-roller xfce4-clipman-plugin xfce4-screenshooter nitrogen neofetch htop discord gsimplecal noto-fonts-emoji cups ghostscript gsfonts avahi gutenprint foomatic-db-gutenprint-ppds system-config-printer gcolor3 lxappearance pavucontrol neovim python-psutil tree picom ulauncher --noconfirm
 
