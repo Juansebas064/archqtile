0c5ce7f74939297c1f96edbe144a63927ac252b3
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-01 22:21:24 -0500

    Fixed script for dual monitor

--- scripts/dual-monitor.sh
+++ scripts/dual-monitor.sh
@@ -2,12 +2,13 @@ source=$(xrandr | grep ' connected' | grep 'HDMI' | awk '{print $1}')
 
 if [ $source = 'HDMI1' ]
 then   
-    xrandr --output eDP1 --mode 1368x768 --pos 1368x0 --output HDMI1 --auto --above eDP1
-    cp ~/archqtile/scripts/bg-saved.cfg ~/archqtile/.config/nitrogen/
+    xrandr --output eDP-1 --mode 1368x768 --pos 1368x0 --output HDMI-1 --auto --above eDP-1
     qtile cmd-obj -o cmd -f restart
+    cp ~/archqtile/scripts/bg-saved.cfg ~/archqtile/.config/nitrogen
     nitrogen --restore
 else
-    xrandr -s 1368x768 --output HDMI1 --off
+    xrandr -s 1368x768 --output HDMI-1 --off
     qtile cmd-obj -o cmd -f restart
-    nitrogen --set-auto ~/archqtile/snow.jpg
+    cp ~/archqtile/scripts/bg-saved.cfg ~/archqtile/.config/nitrogen
+    nitrogen --restore
 fi
